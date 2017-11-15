e0015e2ef4d00b32fe3f4c3988d2e0a0934c2f49
ynpan <ynpan@CNynpan.local>
2017-11-15 21:27:03 +0800

    init project

---
+++ src/main/java/com/thoughtworks/demo/service/Calculate.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.demo.service;
+
+public class Calculate {
+    public int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/thoughtworks/demo/service/CalculateTest.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.demo.service;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculateTest {
+    @Test
+    public void testAdd() throws Exception {
+        Calculate calculate=new Calculate();
+        assertEquals(3,calculate.add(1,2));
+    }
+}
