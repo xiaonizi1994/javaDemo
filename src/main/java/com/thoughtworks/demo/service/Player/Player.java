2d93ea3bf42fb906fd6794c889478ac54e6af1a8
ynpan <ynpan@thoughtworks.com>
2017-12-04 00:05:46 +0800

    add go method to verify that no static method can call static verb

--- src/main/java/com/thoughtworks/demo/service/Player/Player.java
+++ src/main/java/com/thoughtworks/demo/service/Player/Player.java
@@ -1,11 +1,16 @@
 package com.thoughtworks.demo.service.Player;
 
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
 public class Player {
     static int playCount=0;
     private String name;
 
-    private final static int x = 0;
-    private final static int y;
+    protected final static int x = 0;
+    protected final static int y;
 
     static {
         y=1;
@@ -14,5 +19,9 @@ public class Player {
     public Player(String n) {
         name=n;
         playCount++;
+    }
+
+    public void go(){
+        System.out.println(playCount);
     }
 }
--- src/test/java/com/thoughtworks/demo/service/PlayerTest.java
+++ src/test/java/com/thoughtworks/demo/service/PlayerTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.demo.service;
 
 import com.thoughtworks.demo.BaseTest;
+import com.thoughtworks.demo.service.Player.Player;
 import com.thoughtworks.demo.service.Player.PlayerDrive;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PlayerTest extends BaseTest{
     @Autowired
     PlayerDrive playerDrive;
+
+    @Autowired
+    Player player;
     @Test
     void name() {
         playerDrive.main();
         assertEquals("0\n1\n",outContent.toString());
+    }
+
+    @Test
+    void should_return_static_verb_when_no_static_method_call_static_verb() {
+        player.go();
+        assertEquals("1\n",outContent.toString());
     }
 }
