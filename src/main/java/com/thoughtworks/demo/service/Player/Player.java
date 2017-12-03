b0c6a9d8ba15a3e8d324c4406a0c71dfa1471020
ynpan <ynpan@thoughtworks.com>
2017-12-03 23:35:09 +0800

    add static x/y to practice initial final static verb

--- src/main/java/com/thoughtworks/demo/service/Player/Player.java
+++ src/main/java/com/thoughtworks/demo/service/Player/Player.java
@@ -4,6 +4,13 @@ public class Player {
     static int playCount=0;
     private String name;
 
+    private final static int x = 0;
+    private final static int y;
+
+    static {
+        y=1;
+    }
+
     public Player(String n) {
         name=n;
         playCount++;
